04cebbd42b762659966825d100347d32d3a584e6
Shresht Sharma <95879153+shreshtsharma@users.noreply.github.com>
2022-03-23 23:46:32 +0530

    Time: 6 ms (18.89%), Space: 6.6 MB (11.81%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.size()-1;
+        int count=0; 
+         while(s[i]==' ')
+       {
+           i--;
+       }
+        while( i>=0 && s[i]!=' ')
+        {
+            count++;
+            i--;
+            
+        }
+        return count;
+    }
+};
