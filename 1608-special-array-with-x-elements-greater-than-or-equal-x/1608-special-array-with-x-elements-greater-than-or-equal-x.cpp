e888f49f18b5105be2af5f058772caab6e511a8c
Shresht Sharma <95879153+shreshtsharma@users.noreply.github.com>
2022-05-21 21:01:55 +0530

    Time: 6 ms (27.97%), Space: 8.5 MB (14.47%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   int specialArray(vector<int>& nums) {
+		// Brute force
+        for(int i=1;i<1001;i++){
+            int cnt=0;
+            for(auto num: nums){
+                if(i<=num)
+                    cnt++;
+            }
+            if(i == cnt)
+                return i;
+        }
+        return -1;
+	}
+};
