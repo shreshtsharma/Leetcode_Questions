f70c679629a97bfc15abdf01216166c610fd1e21
Shresht Sharma <95879153+shreshtsharma@users.noreply.github.com>
2022-08-28 23:32:30 +0530

    Time: 10 ms (56.59%), Space: 14.2 MB (17.08%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -4,12 +4,11 @@ public:
         sort(candidates.begin(),candidates.end());
         vector<vector<int>> vec;
         vector<int> arr;
-        int rep=INT_MAX;
-        solve(0,vec,arr,candidates,target,rep);
+        solve(0,vec,arr,candidates,target);
         
         return vec;
     }
-    void solve(int ind,vector<vector<int>> &vec,vector<int> arr,vector<int> cand,int tar,int rep)
+    void solve(int ind,vector<vector<int>> &vec,vector<int> arr,vector<int> cand,int tar)
     {
         if(ind==cand.size())
         {
@@ -28,12 +27,13 @@ public:
     
     for(int i=ind;i<cand.size();i++)
     {
-        if(cand[i]!=rep && cand[i]<=tar)
+        if(cand[i]<=tar)
         {
+            if(i>ind && cand[i]==cand[i-1])continue;
+            
             arr.push_back(cand[i]);
-            solve(i+1,vec,arr,cand,tar-cand[i],rep);
+            solve(i+1,vec,arr,cand,tar-cand[i]);
             arr.pop_back();
-            rep=cand[i];
             
         }
     }
