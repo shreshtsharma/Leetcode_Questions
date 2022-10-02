11e37a6842c7b9b7347438b059479c6c1db6106c
Shresht Sharma <95879153+shreshtsharma@users.noreply.github.com>
2022-10-02 22:48:55 +0530

    Time: 375 ms (8.34%), Space: 27.4 MB (6.98%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numTilePossibilities(string tiles) {
+        set<string> st;
+        string str;
+        vector<int> freq(tiles.size(),0);
+        solve(tiles,st,freq,str,0);
+        return st.size();
+    }
+    void solve(string tiles,set<string> &st,vector<int> freq,string str,int ind)
+    {
+        if(ind>tiles.size())
+        {
+            return;
+        }
+        for(int i=0;i<tiles.size();i++)
+        {
+            if(freq[i]==0){
+                freq[i]=1;
+                str.push_back(tiles[i]);
+                st.insert(str);
+                solve(tiles,st,freq,str,ind+1);
+                str.pop_back();
+                freq[i]=0;
+        }
+    }
+    }
+};
