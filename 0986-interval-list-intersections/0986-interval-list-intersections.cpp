ad709539c07fe267d7f8c227ce20f9a2a6fe7055
Shresht Sharma <95879153+shreshtsharma@users.noreply.github.com>
2022-10-29 23:23:12 +0530

    Time: 50 ms (75.22%), Space: 19.8 MB (11.29%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstlist, vector<vector<int>>& secondlist) {
+        int i=0;
+        int j=0;
         vector<vector<int>> res;
-        for(int j=0;j<secondlist.size();j++)
+        while(i<firstlist.size() && j<secondlist.size())
         {
-             for(int i=0; i<firstlist.size();i++)
-            {
+            
             vector<int> vec;
-            if(firstlist[i][0]<secondlist[j][0] && firstlist[i][1]<secondlist[j][0] ||              firstlist[i][0]>secondlist[j][1])
-            {
-                continue;
-            }
-            else
-            {
+            // if(firstlist[i][0]<secondlist[j][0] && firstlist[i][1]<secondlist[j][0] )
+            // {
+            //     i++;
+            // }
+            // if(firstlist[i][0]>secondlist[j][1])
+            // {
+            //     j++;
+            // }
                 int first=max(firstlist[i][0],secondlist[j][0]);
                 int second=min(firstlist[i][1],secondlist[j][1]);
+            if(first<=second){
                 vec.push_back(first);
                 vec.push_back(second);
                  res.push_back(vec);
             }
-           
+                if(firstlist[i][1]>secondlist[j][1])
+                    j++;
+                else
+                    i++;
+        
         }
+        return res;
+        
     }
-             return res;
-    }
-    
 };
